5062f6517ddca7310c3fea57dabb5a75a565e65e
Gord L <rndedg@gmail.com>
2023-05-09 11:17:55 -0700

    Completed assignments within script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,3 +5,18 @@ console.log(["apples", "bananas", "oranges"]);
 
 let firstName = "Gord";
 console.log(`Hello ${firstName}`);
+
+// Assignments
+
+//Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "Canada";
+let continent = "North America";
+let population = 38.25;
+
+console.log(
+  `I live in ${country}, ${continent} which has a population of ${population} million people.`
+);
