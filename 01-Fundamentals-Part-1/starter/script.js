02ad1ddb91280a95344a6123cb6ecac36f54c804
Gord L <rndedg@gmail.com>
2023-05-09 11:48:51 -0700

    Added let, const, var exercises and assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-// Part 1
+// Part 1 - VALUES AND VARIABLES
 
 // let js = "amazing";
 
@@ -8,45 +8,56 @@
 // let firstName = "Gord";
 // console.log(`Hello ${firstName}`);
 
-// Part 2
+// Part 2 - DATA TYPES
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof "Gord");
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Gord");
 
-javascriptIsFun = "YES!";
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
 // typeof known error/bug, unchanged due to legacy reasons
-console.log(typeof null);
+// console.log(typeof null);
+
+// Part 3 - LET, CONST, VAR
+// let can be reassigned, const cannot (immutable)
+// const cannot be declared as an empty value
+// var - should be avoided as it is more of a 'legacy' way of doing things but acts like let
+
+// let age = 30;
+// reassign/mutate variable
+// age = 31;
+
+// const birthYear = 1985;
 
 // Assignments
 
-// VALUES AND VARIABLES
+// Part 1. VALUES AND VARIABLES
 // 1. Declare variables called 'country', 'continent' and 'population' and
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
 
-let country = "Canada";
-let continent = "North America";
-let population = 38.25;
+// const country = "Canada";
+// const continent = "North America";
+// let population = 38.25;
 
-console.log(
-  `I live in ${country}, ${continent} which has a population of ${population} million people.`
-);
+// console.log(
+//   `I live in ${country}, ${continent} which has a population of ${population} million people.`
+// );
 
-// DATA TYPES
+// Part 2. DATA TYPES
 
 // LECTURE: Data Types
 // 1. Declare a variable called 'isIsland' and set its value according to your
@@ -55,10 +66,21 @@ console.log(
 // 2. Log the types of 'isIsland', 'population', 'country' and 'language'
 // to the console
 
-let isIsland = false;
-let language;
+// const isIsland = false;
+// let language;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// Part 3. LET, CONST, VAR
+
+// 1. Set the value of 'language' to the language spoken where you live (some
+//   countries have multiple languages, but just choose one)
+//   2. Think about which variables should be const variables (which values will never
+//   change, and which might change?). Then, change these variables to const.
+//   3. Try to change one of the changed variables now, and observe what happens
+
+// language = "English";
+// console.log(language);
