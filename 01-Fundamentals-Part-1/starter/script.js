dc9bf33466a0273cf238805037788b24e9ffd6c6
Gord L <rndedg@gmail.com>
2023-05-09 11:37:09 -0700

    Added Data Types practice and assignments

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,39 @@
-let js = "amazing";
+// Part 1
 
-console.log(40 + 8 + 23 - 10);
-console.log(["apples", "bananas", "oranges"]);
+// let js = "amazing";
 
-let firstName = "Gord";
-console.log(`Hello ${firstName}`);
+// console.log(40 + 8 + 23 - 10);
+// console.log(["apples", "bananas", "oranges"]);
+
+// let firstName = "Gord";
+// console.log(`Hello ${firstName}`);
+
+// Part 2
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Gord");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// typeof known error/bug, unchanged due to legacy reasons
+console.log(typeof null);
 
 // Assignments
 
-//Values and Variables
+// VALUES AND VARIABLES
 // 1. Declare variables called 'country', 'continent' and 'population' and
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
@@ -20,3 +45,20 @@ let population = 38.25;
 console.log(
   `I live in ${country}, ${continent} which has a population of ${population} million people.`
 );
+
+// DATA TYPES
+
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
