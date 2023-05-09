8c87548fc35f9f0ab0349198ab2ce88d7ff10f7a
Gord L <rndedg@gmail.com>
2023-05-09 11:04:42 -0700

    Little more work with JS

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,7 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 
 console.log(40 + 8 + 23 - 10);
+console.log(["apples", "bananas", "oranges"]);
+
+let firstName = "Gord";
+console.log(`Hello ${firstName}`);
