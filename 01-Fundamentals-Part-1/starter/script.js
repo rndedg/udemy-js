90b60a1e9cccce1dfa62f41713106c25371e2609
Gord L <rndedg@gmail.com>
2023-05-11 11:07:12 -0700

    Added equality operator notes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -167,22 +167,45 @@
 // 4. null
 // 5. NaN
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean("Gord"));
-console.log(Boolean({}));
-console.log(Boolean(24));
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Gord"));
+// console.log(Boolean({}));
+// console.log(Boolean(24));
 
-const money = 0;
-if (money) {
-  console.log("Don't spend it all ;)");
+// const money = 0;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
+// Part 10 - Equality Operators: == VS. ===
+// === strict comparison
+// == loose compaison, has type coercion
+const age = "18";
+if (age === 18) console.log("You just became an adult (strict)"); // if no else statement you don't need the {} block
+if (age == 18) console.log("You just became an adult (loose)");
+// try to avoid loose compaisons and stick to converting before comparison
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 10) {
+  console.log("10 is also a cool number");
 } else {
-  console.log("You should get a job!");
+  console.log("Number is not 23, 7, or 10");
 }
 
-let height;
-if (height) {
-  console.log("YAY! Height is defined");
-} else {
-  console.log("Height is UNDEFINED");
-}
+if (favourite !== 10) console.log("Why not 10?");
