efda83744ba0961bccb8245ae8ee1ebc9673fb1e
Gord L <rndedg@gmail.com>
2023-05-18 09:50:38 -0700

    Added basic Function exercises

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,16 +2,39 @@
 
 // Part 1 - strict mode and reserved words
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) {
-  hasDriversLicense = true;
-}
+// if (passTest) {
+//   hasDriversLicense = true;
+// }
 
-if (hasDriversLicense) {
-  console.log("I can drive :D");
-}
+// if (hasDriversLicense) {
+//   console.log("I can drive :D");
+// }
+
 // Reserved words for possible future features can't be used for variable names
 // const interface = "Audio";
-//const private = 234;
+// const private = 234;
+
+// Functions
+
+// function logger() {
+//   console.log("My name is Gord");
+// }
+
+// calling/running/invoking the function - executing/using the function
+// logger();
+// logger();
+// logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
