fd4086bfa986cabdd9761e3ccf380e9b0b87b3a7
Gord L <rndedg@gmail.com>
2023-05-18 11:28:47 -0700

    Added callback assignment

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -56,3 +56,22 @@ const percentageOfWorld3 = (population) => (population / worldPop) * 100;
 console.log(percentageOfWorld3(38).toFixed(2));
 console.log(percentageOfWorld3(332).toFixed(2));
 console.log(percentageOfWorld3(51).toFixed(2));
+
+// Functions Calling Other Functions
+// 1. Create a function called 'describePopulation'. Use the function type you
+// like the most. This function takes in two arguments: 'country' and
+// 'population', and returns a string like this: 'China has 1441 million people,
+// which is about 18.2% of the world.'
+// 2. To calculate the percentage, 'describePopulation' call the
+// 'percentageOfWorld1' you created earlier
+// 3. Call 'describePopulation' with data for 3 countries of your choice
+
+function describePopulation(country, population) {
+  return `${country} has ${population} million people, which is about ${percentageOfWorld1(
+    population
+  ).toFixed(2)} of the world.`;
+}
+
+console.log(describePopulation("Canada", 38));
+console.log(describePopulation("USA", 332));
+console.log(describePopulation("Colombia", 51));
