5044b92bccd8782356a116aa4da0676f6e6899f1
Gord L <rndedg@gmail.com>
2023-05-19 09:46:03 -0700

    Added Introduction to Arrays.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -115,3 +115,58 @@
 
 // console.log(yearsUntilRetirement(1985, "Gord"));
 // console.log(yearsUntilRetirement(1950, "Dave"));
+
+// Introduction to Arrays
+
+// Will get overwhelming to keep using individual variables
+// const friend1 = "Michael";
+// const friend2 = "Steven";
+// const friend3 = "Peter";
+
+// Literal syntax, most common way of creating an array
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
+
+// const years = new Array(2012, 1985, 2003);
+// console.log(years);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// Will automatically target the last item in an array
+// console.log(friends[friends.length - 1]);
+
+// Only primative values are immutable when set as const
+// Can't replace entire array, only sections/items
+// friends[2] = "Dave";
+// console.log(friends);
+
+// friends = ["Mark", "William"];
+
+// const firstName = "Gord";
+// const gord = [firstName, "Letkeman", 2037 - 1985, "developer", friends];
+
+// console.log(gord);
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2016];
+
+// This works but it is sloppy
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+
+console.log(ages);
