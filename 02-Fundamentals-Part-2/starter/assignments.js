579b4b5a109cfda370e9a4a0f6457777ece921d9
Gord L <rndedg@gmail.com>
2023-05-23 10:40:31 -0700

    Added Intro to Arrays and Basic Array Operator assignments.

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -75,3 +75,54 @@ function describePopulation(country, population) {
 console.log(describePopulation("Canada", 38));
 console.log(describePopulation("USA", 332));
 console.log(describePopulation("Colombia", 51));
+
+// Introduction to Arrays
+// 1. Create an array containing 4 population values of 4 countries of your choice.
+// You may use the values you have been using previously. Store this array into a
+// variable called 'populations'
+// 2. Log to the console whether the array has 4 elements or not (true or false)
+// 3. Create an array called 'percentages' containing the percentages of the
+// world population for these 4 population values. Use the function
+// 'percentageOfWorld1' that you created earlier to compute the 4
+// percentage values
+
+const populations = [35, 122, 20, 255];
+console.log(populations.length === 4);
+
+const percentages = [
+  populations.map((x) => Number(percentageOfWorld1(x)).toFixed(2)),
+];
+console.log(percentages);
+
+// Basic Array Operators
+
+// 1. Create an array containing all the neighbouring countries of a country of your
+// choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+// into a variable called 'neighbours'
+// 2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+// your selected country. So add it to the end of the 'neighbours' array
+// 3. Unfortunately, after some time, the new country is dissolved. So remove it from
+// the end of the array
+// 4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+// console: 'Probably not a central European country :D'
+// 5. Change the name of one of your neighbouring countries. To do that, find the
+// index of the country in the 'neighbours' array, and then use that index to
+// change the array at that index position. For example, you can search for
+// 'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+const neighbours = ["Mexico", "Canada"];
+console.log(neighbours);
+
+neighbours.push("Utopia");
+console.log(neighbours);
+
+neighbours.pop();
+console.log(neighbours);
+
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not a central European country :D");
+}
+
+neighbours[neighbours.indexOf("Canada")] = "Canada Rocks!";
+
+console.log(neighbours);
