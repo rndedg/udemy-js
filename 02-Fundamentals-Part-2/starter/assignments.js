9bfdde31675fef94e61f299597f0d1a26829461d
Gord L <rndedg@gmail.com>
2023-05-24 10:01:44 -0700

    Added Object and Dot vs. Bracket Notation assignments.

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -126,3 +126,33 @@ if (!neighbours.includes("Germany")) {
 neighbours[neighbours.indexOf("Canada")] = "Canada Rocks!";
 
 console.log(neighbours);
+
+// Introduction to Objects
+// 1. Create an object called 'myCountry' for a country of your choice, containing
+// properties 'country', 'capital', 'language', 'population' and
+// 'neighbours' (an array like we used in previous assignments)
+
+const myCountry = {
+  country: "Canada",
+  capital: "Ottawa",
+  language: "English",
+  population: 38,
+  neighbours: ["USA"],
+};
+console.log(myCountry);
+
+// Dot vs. Bracket Notation
+// 1. Using the object from the previous assignment, log a string like this to the
+// console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+// and a capital called Helsinki.'
+// 2. Increase the country's population by two million using dot notation, and then
+// decrease it by two million using brackets notation.
+
+console.log(
+  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neigbouring country and a capital called ${myCountry.capital}.`
+);
+
+myCountry.population += 2;
+console.log(myCountry.population);
+myCountry["population"] -= 2;
+console.log(myCountry.population);
