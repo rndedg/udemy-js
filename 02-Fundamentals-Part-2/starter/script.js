a47f61d1a4c260dcf3ee2cd11c16253f3fe3a417
Gord L <rndedg@gmail.com>
2023-05-24 12:08:55 -0700

    Added while loop notes and exercises

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -349,14 +349,14 @@
 //   console.log(gord[i], typeof gord[i]);
 // }
 
-const gord = [
-  "Gord",
-  "Letkeman",
-  2037 - 1985,
-  "developer",
-  ["Mike", "Shane", "Chris"],
-  true,
-];
+// const gord = [
+//   "Gord",
+//   "Letkeman",
+//   2037 - 1985,
+//   "developer",
+//   ["Mike", "Shane", "Chris"],
+//   true,
+// ];
 
 // Reverse loop
 
@@ -366,10 +366,31 @@ const gord = [
 
 // Nested loop
 
-for (let exercise = 1; exercise < 4; exercise++) {
-  console.log(`---- Starting exercise${exercise} ----`);
+// for (let exercise = 1; exercise < 4; exercise++) {
+//   console.log(`---- Starting exercise${exercise} ----`);
 
-  for (let rep = 1; rep < 6; rep++) {
-    console.log(`Exercise ${exercise}: Lifting weight repitition ${rep}`);
-  }
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(`Exercise ${exercise}: Lifting weight repitition ${rep}`);
+//   }
+// }
+
+// While Loop
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repitition ${rep}`);
+// }
+
+// let rep = 1;
+
+// while (rep <= 10) {
+//   console.log(`WHILE: Lifting weights repitition ${rep}`);
+//   rep++;
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("You rolled a 6, finally!");
 }
