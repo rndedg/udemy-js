7011cc2cbf6c2b4a30d70ffb87d5065ef1f37ee6
Gord L <rndedg@gmail.com>
2023-05-24 12:15:04 -0700

    Added Looping arrays assignment

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -181,6 +181,22 @@ console.log(myCountry.checkIsland());
 // Use a for loop to simulate the 50 people voting, by logging a string like this to
 // the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
 
-for (let voter = 1; voter <= 50; voter++) {
-  console.log(`Voter Number ${voter} is currently voting.`);
+// for (let voter = 1; voter <= 50; voter++) {
+//   console.log(`Voter Number ${voter} is currently voting.`);
+// }
+
+// Looping Arrays, Breaking and Continuing
+// 1. Let's bring back the 'populations' array from a previous assignment
+// 2. Use a for loop to compute an array called 'percentages2' containing the
+// percentages of the world population for the 4 population values. Use the
+// function 'percentageOfWorld1' that you created earlier
+// 3. Confirm that 'percentages2' contains exactly the same values as the
+// 'percentages' array that we created manually in the previous assignment,
+// and reflect on how much better this solution is
+
+let percentages2 = [];
+for (let i = 0; i < populations.length; i++) {
+  percentages2.push(percentageOfWorld1(populations[i]).toFixed(2));
 }
+
+console.log(percentages2);
