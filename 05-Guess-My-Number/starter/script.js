6ce180da31086eaa1df80ecee44b89ce09fb1d80
Gord L <rndedg@gmail.com>
2023-05-26 10:26:00 -0700

    Practice selecting and editing elements in dom using querySelector.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+// Log what text is in current object with class of message
+console.log(document.querySelector('.message').textContent);
+
+// select object with message class and set text content to new text.
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
